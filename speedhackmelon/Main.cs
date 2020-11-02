8b3b8484068645ac5538414ad449ad69855c4b36
hemidemisemipresent <57880110+hemisemidemipresent@users.noreply.github.com>
2020-11-02 19:46:41 +0800

    removed unnecessary imports

--- speedhackmelon/Main.cs
+++ speedhackmelon/Main.cs
@@ -1,30 +1,7 @@
-﻿using MelonLoader;
-using Harmony;
-using NKHook6.Api;
-using Assets.Scripts.Unity.UI_New.InGame.Races;
-using Assets.Scripts.Simulation.Towers.Weapons;
+using MelonLoader;
 using NKHook6;
-using Assets.Scripts.Simulation;
-using Assets.Scripts.Unity.UI_New.InGame;
-using NKHook6.Api.Extensions;
-using Assets.Scripts.Unity.UI_New.Main;
 using NKHook6.Api.Events;
-using Assets.Scripts.Simulation.Bloons;
-using Assets.Scripts.Models.Towers;
-using NKHook6.Api.Utilities;
-using Assets.Scripts.Unity;
-
-using NKHook6.Api.Enums;
-
-using static NKHook6.Api.Events._Towers.TowerEvents;
-using Assets.Scripts.Simulation.Towers;
-
-using static NKHook6.Api.Events._Weapons.WeaponEvents;
 using Assets.Scripts.Utils;
-
-using static NKHook6.Api.Events._TimeManager.TimeManagerEvents;
-using Il2CppSystem.Collections;
-using NKHook6.Api.Events._Bloons;
 
 namespace speedhackmelon
 {
@@ -47,8 +24,6 @@ namespace speedhackmelon
             TimeManager.maxSimulationStepsPerUpdate = speed;
 
         }
-
-
 
         [EventAttribute("KeyPressEvent")]
         public static void onEvent(KeyEvent e)
@@ -77,12 +52,7 @@ namespace speedhackmelon
                 Logger.Log("speed = 100");
             }
 
-
-
         }
-
-
-
 
     }
 
